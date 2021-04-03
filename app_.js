b54a46e6dad3a44841152aa25b5dd5059ab383a2
NikolaiMD <44241560+NikolaiMD@users.noreply.github.com>
2021-04-04 02:28:17 +0300

    Add files via upload

--- app_.js
+++ app_.js
@@ -6,11 +6,7 @@ async function readFromFile(fileName) {
 
 
 async function getData(path, propertyName) {
-    return await readFromFile(path).then(result => {
-        if (result.hasOwnProperty(propertyName)) {
-            return result[propertyName]
-        }
-    })
+    return await readFromFile(path).then(result => result.hasOwnProperty(propertyName) ? result[propertyName] : 'No such data!')
 }
 
 
@@ -18,7 +14,6 @@ async function printValue(path, name) {
     return await getData(path, name).then(result => console.log(result))
 }
 
-//
 // ////////////////////////////////
 printValue('./data.json', 'dummy')
 printValue('./data.json', 'inexistent')
